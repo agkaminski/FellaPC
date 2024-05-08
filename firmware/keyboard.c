50e432cbe95e5a8e998f3d1e6d73154b96cd8f2d
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2024-05-08 23:26:36 +0200

    fw: keyboard simplyfication

--- firmware/keyboard.c
+++ firmware/keyboard.c
@@ -11,66 +11,43 @@
 #include "keys.h"
 #include "keyboard.h"
 
-#define K(main, alt) (((alt) << 8) | ((main) & 0xff))
-#define MAIN(key) ((key) & 0xff)
-#define ALT(key) (((key) >> 8) & 0xff)
-
 extern void keyboard_scanLow(uint8_t *row);
 
 struct keys keyboard_keys = { 0 };
 
-static const uint16_t map[5][16] = {
+const uint8_t keymap_main[5][16] = {
 {
-	K(KEY_ESC, KEY_GRAVE), K(KEY_1, KEY_F1), K(KEY_2, KEY_F2),
-	K(KEY_3, KEY_F3), K(KEY_4, KEY_F4), K(KEY_5, KEY_F5), K(KEY_6, KEY_F6),
-	K(KEY_7, KEY_F7), K(KEY_8, KEY_F8), K(KEY_9, KEY_F9), K(KEY_0, KEY_F10),
-	K(KEY_MINUS, KEY_F11), K(KEY_EQUAL, KEY_F12),
-	K(KEY_BACKSPACE, KEY_BACKSPACE), K(KEY_HOME, KEY_END)
+	KEY_ESC, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0,
+	KEY_MINUS, KEY_EQUAL, KEY_BACKSPACE, KEY_HOME
 },
 {
-	K(KEY_TAB, KEY_TAB), K(KEY_Q, KEY_Q), K(KEY_W, KEY_W), K(KEY_E, KEY_E),
-	K(KEY_R, KEY_R), K(KEY_T, KEY_T), K(KEY_Y, KEY_Y), K(KEY_U, KEY_U),
-	K(KEY_I, KEY_I), K(KEY_O, KEY_O), K(KEY_P, KEY_P),
-	K(KEY_LEFTBRACE, KEY_LEFTBRACE), K(KEY_RIGHTBRACE, KEY_RIGHTBRACE),
-	K(KEY_BACKSLASH, KEY_BACKSLASH), K(KEY_DELETE, KEY_DELETE)
+	KEY_TAB, KEY_Q, KEY_W, KEY_E, KEY_R, KEY_T, KEY_Y, KEY_U, KEY_I, KEY_O, KEY_P,
+	KEY_LEFTBRACE, KEY_RIGHTBRACE, KEY_BACKSLASH, KEY_DELETE
 },
 {
-	K(KEY_CAPSLOCK, KEY_CAPSLOCK), K(KEY_A, KEY_A), K(KEY_S, KEY_S),
-	K(KEY_D, KEY_D), K(KEY_F, KEY_F), K(KEY_G, KEY_G), K(KEY_H, KEY_H),
-	K(KEY_J, KEY_J), K(KEY_K, KEY_K), K(KEY_L, KEY_L),
-	K(KEY_SEMICOLON, KEY_SEMICOLON), K(KEY_APOSTROPHE, KEY_APOSTROPHE),
-	K(KEY_NONE, KEY_NONE), K(KEY_ENTER, KEY_ENTER),
-	K(KEY_PAGEUP, KEY_PAGEUP)
+	KEY_CAPSLOCK, KEY_A, KEY_S, KEY_D, KEY_F, KEY_G, KEY_H, KEY_J, KEY_K, KEY_L,
+	KEY_SEMICOLON, KEY_APOSTROPHE, KEY_NONE, KEY_ENTER, KEY_PAGEUP
 },
 {
-	K(KEY_LEFTSHIFT, KEY_LEFTSHIFT), K(KEY_Z, KEY_Z), K(KEY_X, KEY_X),
-	K(KEY_C, KEY_C), K(KEY_V, KEY_V), K(KEY_B, KEY_B), K(KEY_N, KEY_N),
-	K(KEY_M, KEY_M), K(KEY_COMMA, KEY_COMMA), K(KEY_DOT, KEY_DOT),
-	K(KEY_SLASH, KEY_SLASH), K(KEY_NONE, KEY_NONE), K(KEY_RIGHTSHIFT, KEY_RIGHTSHIFT),
-	K(KEY_UP, KEY_UP), K(KEY_PAGEDOWN, KEY_PAGEDOWN)
+	KEY_LEFTSHIFT, KEY_Z, KEY_X, KEY_C, KEY_V, KEY_B, KEY_N, KEY_M, KEY_COMMA, KEY_DOT,
+	KEY_SLASH, KEY_NONE, KEY_RIGHTSHIFT, KEY_UP, KEY_PAGEDOWN
 },
 {
-	K(KEY_LEFTCTRL, KEY_LEFTCTRL), K(KEY_LEFTMETA, KEY_LEFTMETA),
-	K(KEY_LEFTALT, KEY_LEFTALT), K(KEY_NONE, KEY_NONE),
-	K(KEY_NONE, KEY_NONE), K(KEY_SPACE, KEY_SPACE),
-	K(KEY_NONE, KEY_NONE), K(KEY_NONE, KEY_NONE),
-	K(KEY_RIGHTALT, KEY_RIGHTALT), K(KEY_NONE, KEY_NONE),
-	K(KEY_RIGHTCTRL, KEY_RIGHTCTRL), K(KEY_FN, KEY_FN), K(KEY_LEFT, KEY_LEFT),
-	K(KEY_DOWN, KEY_DOWN), K(KEY_RIGHT, KEY_RIGHT)
+	KEY_LEFTCTRL, KEY_LEFTMETA, KEY_LEFTALT, KEY_NONE, KEY_NONE, KEY_SPACE, KEY_NONE,
+	KEY_NONE, KEY_RIGHTALT, KEY_NONE, KEY_RIGHTCTRL, KEY_FN, KEY_LEFT, KEY_DOWN, KEY_RIGHT
 } };
+
+/* Row 0 only */
+const uint8_t keymap_alt[16] = { KEY_GRAVE, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5,
+	KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12, KEY_BACKSPACE, KEY_END };
 
 static uint8_t getModifier(uint8_t key)
 {
-	switch (key) {
-		case KEY_LEFTSHIFT:  return KEY_MOD_LSHIFT;
-		case KEY_RIGHTSHIFT: return KEY_MOD_RSHIFT;
-		case KEY_LEFTCTRL:   return KEY_MOD_LCTRL;
-		case KEY_LEFTALT:    return KEY_MOD_LALT;
-		case KEY_RIGHTALT:   return KEY_MOD_RALT;
-		case KEY_RIGHTCTRL:  return KEY_MOD_RCTRL;
-		case KEY_LEFTMETA:   return KEY_MOD_LMETA;
-		default:             return 0;
+	if ((key < KEY_LEFTCTRL) || (key == KEY_FN)) {
+		return 0;
 	}
+
+	return 1 << (key & 0xf);
 }
 
 int8_t keyboard_scan(void)
@@ -87,16 +64,20 @@ int8_t keyboard_scan(void)
 
 	for (i = 0; i < 16; ++i) {
 		uint8_t change = row[i] ^ lastState[i];
-
 		lastState[i] = row[i];
 
 		if (change) {
 			uint8_t j;
 			for (j = 0; j < 5; ++j) {
 				if (change & (1 << j)) {
-					uint8_t key = isFn ? ALT(map[j][i]) : MAIN(map[j][i]);
+					uint8_t key = keymap_main[j][i];
 					uint8_t modifier = getModifier(key);
-					uint8_t state = !!(row[i] & (1 << j));
+					uint8_t state = row[i] & (1 << j);
+
+					if (isFn && !j) {
+						key = keymap_alt[i];
+					}
+
 					if (modifier) {
 						if (state)
 							keyboard_keys.mod |= modifier;
@@ -104,27 +85,18 @@ int8_t keyboard_scan(void)
 							keyboard_keys.mod &= ~modifier;
 					}
 					else {
-						int8_t ret = 0;
-						if (row[i] & (1 << j)) {
-							if (keyboard_keys.key != KEY_NONE) {
-								ret = -1;
-							}
-							keyboard_keys.key = key;
-						}
-						else {
-							if ((keyboard_keys.key != MAIN(map[j][i])) &&
-									(keyboard_keys.key != ALT(map[j][i]))) {
-										ret = -1;
-							}
-							keyboard_keys.key = KEY_NONE;
+						if (!state) {
+							key = KEY_NONE;
 						}
 
-						if (ret < 0) {
+						if (state && (keyboard_keys.key != KEY_NONE)) {
 							/* Overflow - reset everything to allow reliable recovery */
 							memset(&keyboard_keys, 0, sizeof(keyboard_keys));
 							memset(lastState, 0, sizeof(lastState));
-							return ret;
+							return -1;
 						}
+
+						keyboard_keys.key = key;
 					}
 				}
 			}
--- firmware/keyboard_low.s
+++ firmware/keyboard_low.s
@@ -1,17 +1,17 @@
 ; FellaPC low level keyboard scan
 ; A.K. 2024
 
+.importzp		ptr1, ptr2
+.export			_keyboard_scanLow
+
+.segment "CODE"
+
 ; Has to be in asm! cc65 generates (zp), Y addressing
 ; mode, which reads some random memory during execution.
 ; It's ok for memory access, but disastrous for I/O.
 ; It caused VGA registers to be modified (as some
 ; of them do not handle reads and access is always
 ; treated as write).
-
-.export			_keyboard_scanLow
-.importzp		ptr1, ptr2
-
-.segment		"CODE"
 
 .proc			_keyboard_scanLow: near
 
