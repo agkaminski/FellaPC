fe1a9e0400467f5ce4d0b01e7cab563700ccb5e1
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2024-05-08 23:27:18 +0200

    fw: rewrote keyboard in asm

--- firmware/keyboard.c
+++ firmware/keyboard.c
@@ -11,8 +11,6 @@
 #include "keys.h"
 #include "keyboard.h"
 
-extern void keyboard_scanLow(uint8_t *row);
-
 struct keys keyboard_keys = { 0 };
 
 const uint8_t keymap_main[5][16] = {
@@ -40,68 +38,3 @@ const uint8_t keymap_main[5][16] = {
 /* Row 0 only */
 const uint8_t keymap_alt[16] = { KEY_GRAVE, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5,
 	KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12, KEY_BACKSPACE, KEY_END };
-
-static uint8_t getModifier(uint8_t key)
-{
-	if ((key < KEY_LEFTCTRL) || (key == KEY_FN)) {
-		return 0;
-	}
-
-	return 1 << (key & 0xf);
-}
-
-int8_t keyboard_scan(void)
-{
-	/* Check Fn key before, so we know what to do */
-	int8_t isFn;
-	static uint8_t lastState[16];
-	uint8_t row[16];
-	uint8_t i;
-
-	keyboard_scanLow(row);
-
-	isFn = (row[9] & (1 << 4));
-
-	for (i = 0; i < 16; ++i) {
-		uint8_t change = row[i] ^ lastState[i];
-		lastState[i] = row[i];
-
-		if (change) {
-			uint8_t j;
-			for (j = 0; j < 5; ++j) {
-				if (change & (1 << j)) {
-					uint8_t key = keymap_main[j][i];
-					uint8_t modifier = getModifier(key);
-					uint8_t state = row[i] & (1 << j);
-
-					if (isFn && !j) {
-						key = keymap_alt[i];
-					}
-
-					if (modifier) {
-						if (state)
-							keyboard_keys.mod |= modifier;
-						else
-							keyboard_keys.mod &= ~modifier;
-					}
-					else {
-						if (!state) {
-							key = KEY_NONE;
-						}
-
-						if (state && (keyboard_keys.key != KEY_NONE)) {
-							/* Overflow - reset everything to allow reliable recovery */
-							memset(&keyboard_keys, 0, sizeof(keyboard_keys));
-							memset(lastState, 0, sizeof(lastState));
-							return -1;
-						}
-
-						keyboard_keys.key = key;
-					}
-				}
-			}
-		}
-	}
-
-	return 0;
-}
--- firmware/keyboard_low.s
+++ firmware/keyboard_low.s
@@ -1,30 +1,174 @@
 ; FellaPC low level keyboard scan
 ; A.K. 2024
 
-.importzp		ptr1, ptr2
-.export			_keyboard_scanLow
+.import			_keymap_main, _keymap_alt
+.import			_keyboard_keys
 
-.segment "CODE"
+.importzp		ptr1, ptr2, ptr3, tmp1, tmp2, tmp3, tmp4
 
-; Has to be in asm! cc65 generates (zp), Y addressing
-; mode, which reads some random memory during execution.
-; It's ok for memory access, but disastrous for I/O.
-; It caused VGA registers to be modified (as some
-; of them do not handle reads and access is always
-; treated as write).
+.define			i tmp1
+.define			j tmp2
+.define			isFn ptr3
+.define			change tmp3
+.define			state tmp4
+.define			main ptr1
+.define			alt ptr2
 
-.proc			_keyboard_scanLow: near
+.export 		_keyboard_scan
 
-				STA ptr1
-				STX ptr1 + 1
+.proc			_keyboard_scan: near
+
+.segment		"BSS"
+
+rowLast:		.res 16, $00
+row:			.res 16, $00
+
+.segment		"CODE"
+
+				; scan the keyboard
 				LDY #15
-
-@loop:			LDA $9000, Y
+@scan:			LDA $9000, Y
 				EOR #$FF
-				STA (ptr1), Y
+				STA row, Y
 				DEY
-				BPL @loop
+				BPL @scan
 
+				; prepare map pointers
+				LDA #<_keymap_main
+				STA main
+				LDA #>_keymap_main
+				STA main + 1
+				LDA #<_keymap_alt
+				STA alt
+				LDA #>_keymap_alt
+				STA alt + 1
+
+				; is Fn pressed?
+				LDA row + 9
+				AND #$10
+				STA isFn
+
+				; row iterator
+				LDA #0
+				STA i
+
+@rowLoop:		; change = row ^ rowLast
+				LDY i
+				LDA row, Y
+				EOR rowLast, Y
+				STA change
+				LDA row, Y
+				STA rowLast, Y
+
+				; for each column
+				LDA #0
+				STA j
+
+@colLoop:		; !change[j] continue
+				LDA change
+				AND #1
+				BEQ @colLoopEnd
+
+				; pressed or released?
+				LDY i
+				LDA row, Y
+				AND #1
+				STA state
+
+@getCode:		; get key code (map[j][i])
+				LDY j
+				LDA #0
+				CLC
+@mul:			DEY
+				BMI @mulend
+				ADC #16
+				BNE @mul
+
+@mulend:		ADC i
+				TAY
+
+				; check if Fn is active, change map if so
+				LDA j
+				BNE @noFn
+				LDA isFn
+				BEQ @noFn
+
+				LDA (alt), Y
+				JMP @gotCode
+
+@noFn:			LDA (main), Y
+
+				; is modifier?
+@gotCode:		CMP #$E0
+				BCC @notMod
+				CMP #$FF
+				BEQ @notMod
+
+				; convert key to modifier
+				AND #$0f
+				TAX
+				LDA #1
+@modloop:		CPX #0
+				BEQ @modloopend
+				ASL
+				DEX
+				BNE @modloop
+
+@modloopend:	; if key active
+				LDY state
+				BEQ @modDisable
+				ORA _keyboard_keys
+
+@modEnd:		STA _keyboard_keys
+				JMP @colLoopEnd
+
+@modDisable:	EOR #$FF
+				AND _keyboard_keys
+				JMP @modEnd
+
+@notMod:		; if key active
+				LDY state
+				BNE @active
+
+				LDA #0
+				STA _keyboard_keys + 1
+				JMP @colLoopEnd
+
+@active:		LDY _keyboard_keys + 1
+				BNE @overflow
+				STA _keyboard_keys + 1
+
+@colLoopEnd:	INC j
+				LDX i
+				LSR row, X
+				LSR change
+				BNE @colLoop
+
+				INC i
+
+				; i < 16?
+				LDA i
+				CMP #16
+				BEQ @end
+
+				JMP @rowLoop
+
+@end:			LDA #0
+				TAX
+				RTS
+
+@overflow:		; overflow - clear everything
+				LDA #0
+				LDY #15
+				STA _keyboard_keys
+				STA _keyboard_keys + 1
+
+@ovflLoop:		STA rowLast, Y
+				DEY
+				BPL @ovflLoop
+
+				LDA #$FF
+				TAX
 				RTS
 
 .endproc
