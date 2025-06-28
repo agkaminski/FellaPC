f7f017385416c702b10dce99eb87ad6a6e2fa7fe
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2025-06-28 11:46:20 +0200

    vga/cpld: fix sync pulse polarisation

--- vga/cpld/vga.v
+++ vga/cpld/vga.v
@@ -62,9 +62,9 @@ always @(posedge pclk, negedge rst_n) begin
 		hsync <= 0;
 	else begin
 		if (col_i >= (640 + 16 + 8 - 1) && col_i < (640 + 16 + 8 + 96 - 1))
-			hsync <= 1;
-		else
 			hsync <= 0;
+		else
+			hsync <= 1;
 	end
 end
 
@@ -74,9 +74,9 @@ always @(posedge pclk, negedge rst_n) begin
 		vsync <= 0;
 	else begin
 		if (row_i >= (480 + 10) && row_i < (480 + 10 + 2))
-			vsync <= 1;
-		else
 			vsync <= 0;
+		else
+			vsync <= 1;
 	end
 end
 
